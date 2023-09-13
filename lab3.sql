49d0910f67e4128cff63cf8b59100c27462084a4
hbrown-22 <143039095+hbrown-22@users.noreply.github.com>
2023-09-13 18:01:19 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -21,12 +21,14 @@ values ('s1@jmu.edu','c1'),
 		('s4@jmu.edu','c2'),
 		('s2@jmu.edu','c3'),
 		('s3@jmu.edu','c3')
---q2.5
+--q2.5 need to modify the professor table first because of the email.
 insert into professor(p_email,p_name,office)
 values ('p3@jmu.edu','p3','o3') ;
 
 insert into course(c_number,c_name,room,p_email)
 values ('c4','facebook','r1','p3@jmu.edu')
---need to modify the professor table first because of the email.
 
 --q2.6 need to modify the course table first because it won't work the other way around.
+update course
+set p_email = 'p3@jmu.edu'
+where p_email = 'p1@jmu.edu'
